4dea78f24dabcceceae09a9dce01f07479de53a1
frisdiandi <frisdiandiseptiatama@gmail.com>
2025-04-29 09:05:55 +0700

    E-Monev V1

---
+++ routes/web.php
@@ -0,0 +1,90 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Admin\OPDController;
+use App\Http\Controllers\Admin\AccountController;
+use App\Http\Controllers\Admin\AplikasiController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Clear All:
+Route::get('/clear', function() {
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('optimize');
+    $exitCode = Artisan::call('route:cache');
+    $exitCode = Artisan::call('route:clear');
+    $exitCode = Artisan::call('view:clear');
+    $exitCode = Artisan::call('config:cache');
+    return '<h1>Berhasil dibersihkan</h1>';
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Authentication
+Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Dashboard
+Route::get('/keluar', [HomeController::class, 'keluar']);
+Route::get('/admin/home', [HomeController::class, 'index']);
+Route::get('/admin/home/filter/{id}', [HomeController::class, 'index_filter']);
+Route::get('/admin/change', [HomeController::class, 'change']);
+Route::post('/admin/change_password', [HomeController::class, 'change_password']);
+
+// OPD
+Route::prefix('admin/opd')
+    ->name('admin.opd.')
+    ->middleware('cekLevel:1,2,3')
+    ->controller(OPDController::class)
+    ->group(function () {
+        Route::get('/', 'read')->name('read');
+        Route::get('/add', 'add')->name('add');
+        Route::post('/create', 'create')->name('create');
+        Route::get('/edit/{id}', 'edit')->name('edit');
+        Route::post('/update/{id}', 'update')->name('update');
+        Route::get('/delete/{id}', 'delete')->name('delete');
+    });
+
+// Account
+Route::prefix('admin/account')
+    ->name('admin.account.')
+    ->middleware('cekLevel:1,2,3')
+    ->controller(AccountController::class)
+    ->group(function () {
+        Route::get('/', 'read')->name('read');
+        Route::get('/add', 'add')->name('add');
+        Route::post('/create', 'create')->name('create');
+        Route::get('/edit/{id}', 'edit')->name('edit');
+        Route::post('/update/{id}', 'update')->name('update');
+        Route::get('/delete/{id}', 'delete')->name('delete');
+        Route::get('/reset/{id}', 'reset')->name('reset');
+    });
+
+// Aplikasi
+Route::prefix('admin/aplikasi')
+    ->name('admin.aplikasi.')
+    ->middleware('cekLevel:1,2,3')
+    ->controller(AplikasiController::class)
+    ->group(function () {
+        Route::get('/', 'read')->name('read');
+        Route::get('/add', 'add')->name('add');
+        Route::post('/create', 'create')->name('create');
+        Route::get('/edit/{id}', 'edit')->name('edit');
+        Route::post('/update/{id}', 'update')->name('update');
+        Route::get('/delete/{id}', 'delete')->name('delete');
+    });
